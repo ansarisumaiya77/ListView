8d1ba69d361393f4c8c655dcd4b4002bd9acda16
Sumaiya Rashid <ansarisumaiya77@gmail.com>
2019-12-20 16:44:02 +0500

    Completed

--- app/src/main/java/com/example/listviewpractice/MainActivity.java
+++ app/src/main/java/com/example/listviewpractice/MainActivity.java
@@ -18,16 +18,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         list = (ListView) findViewById(R.id.listView);
 
-        ContactList = new ArrayList<>();
-        ContactList.add(new Contact("Sumaiya Rashid","+923332849115"));
-        ContactList.add(new Contact("Sumaiya Rashid","+923332849115"));
-        ContactList.add(new Contact("Sumaiya Rashid","+923332849115"));
-        ContactList.add(new Contact("Sumaiya Rashid","+923332849115"));
-        ContactList.add(new Contact("Sumaiya Rashid","+923332849115"));
-        ContactList.add(new Contact("Sumaiya Rashid","+923332849115"));
-        ContactList.add(new Contact("Sumaiya Rashid","+923332849115"));
-        ContactList.add(new Contact("Sumaiya Rashid","+923332849115"));
-        ContactList.add(new Contact("Sumaiya Rashid","+923332849115"));
+        ContactList = new ArrayList<Contact>();
+        ContactList.add(new Contact("Sumaiya Rashid","+923332849115",R.drawable.screenshot));
+        ContactList.add(new Contact("Asad Jawaid","+923332849115",R.drawable.screenshot));
+        ContactList.add(new Contact("Maram Sharfuddin","+923332849115",R.drawable.screenshot));
+        ContactList.add(new Contact("Baber Akram","+923332849115",R.drawable.screenshot));
+        ContactList.add(new Contact("Faiza Baber","+923332849115",R.drawable.screenshot));
+        ContactList.add(new Contact("Komal Anees","+923332849115",R.drawable.screenshot));
+        ContactList.add(new Contact("Moiz Baig","+923332849115",R.drawable.screenshot));
+        ContactList.add(new Contact("Wahaj Baig","+923332849115",R.drawable.screenshot));
+        ContactList.add(new Contact("Tasneem Zaki","+923332849115",R.drawable.screenshot));
+        ContactList.add(new Contact("Tooba Zafar","+923332849115",R.drawable.screenshot));
 
         CustomAdapter adapter = new CustomAdapter(this, ContactList);
         list.setAdapter(adapter);
